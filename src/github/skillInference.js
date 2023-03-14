179db20e43856ad6fa56f5ec363708831dd4e101
AurelliaChristie <aurelliachristie77@gmail.com>
2023-03-14 23:55:09 +0700

    skillInference enhancement

--- src/github/skillInference.js
+++ src/github/skillInference.js
@@ -1,10 +1,9 @@
 import endpoints from "../endpoints.json";
-import {toString} from 'nlcst-to-string'
-import {retext} from 'retext'
-import retextPos from 'retext-pos'
-import retextKeywords from 'retext-keywords'
-import natural from 'natural';
-import stopwords from 'stopwords';
+import { toString } from "nlcst-to-string";
+import { retext } from "retext";
+import retextPos from "retext-pos";
+import retextKeywords from "retext-keywords";
+import stopwords from "stopwords";
 
 const githubLink = endpoints["github"];
 
@@ -15,65 +14,66 @@ const skillInference = async (
   token = null,
   top_language_n = 3
 ) => {
+  let decodeBio, decodeReadme;
 
-  //Bio data
-
-  //Check if bio is null or not
-  if (bio == null){
-      var decodeBio = ""
-  } else {
-      var decodeBio = bio.replace(/\\n|###|'|ð|http[s]?:\/\/\S+|[\(\[].*?[\)\]]|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/g, '').toLowerCase();
+  // bio
+  if (bio) {
+    decodeBio = bio
+      .replace(
+        /\\n|###|'|ð|http[s]?:\/\/\S+|[\(\[].*?[\)\]]|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/g,
+        ""
+      )
+      .toLowerCase();
   }
 
-  // ReadMe data
+  const fileBio = await retext()
+    .use(retextPos)
+    .use(retextKeywords)
+    .process(await decodeBio);
+
+  let keywordsArray = fileBio.data.keywords
+    .map((keyword) => toString(keyword.matches[0].node))
+    .filter((word) => !stopwords.english.includes(word));
+
+  let keyphrasesArray = fileBio.data.keyphrases
+    .map((phrase) => phrase.matches[0].nodes.map((d) => toString(d)).join(""))
+    .filter((word) => !stopwords.english.includes(word));
+
+  // readme
   const responseReadme = await fetch(
     `${githubLink}/repos/${githubHandle}/${githubHandle}/contents/README.md`
   );
-
   const dataReadme = await responseReadme.json();
-
-  //Check if there is a readme.md
-  if (dataReadme.message && dataReadme.message === "Not Found") {
-      var decodeReadme = ""
-  } else {
-      var decodeReadme = atob(dataReadme.content).replace(/\\n|###|'|ð|http[s]?:\/\/\S+|[\(\[].*?[\)\]]|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/g, '').toLowerCase();
+  if (dataReadme) {
+    decodeReadme = atob(dataReadme.content)
+      .replace(
+        /\\n|###|'|ð|http[s]?:\/\/\S+|[\(\[].*?[\)\]]|<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/g,
+        ""
+      )
+      .toLowerCase();
   }
-  
-  // Extract profile
 
-  // Extract keywords and keyphrases from readme
-  const file = await retext()
-        .use(retextPos) // Make sure to use `retext-pos` before `retext-keywords`.
-        .use(retextKeywords)
-        .process(await decodeReadme)
-
-  const tokenizer = new natural.WordTokenizer();
-
-  var keywordsArray = file.data.keywords.map((keyword) => {
-      return toString(keyword.matches[0].node);
-    }).filter(word => !stopwords.english.includes(word));
-
-  var keyphrasesArray = file.data.keyphrases.map((phrase) => {
-      return phrase.matches[0].nodes.map((d) => toString(d)).join('');
-    }).filter(word => !stopwords.english.includes(word));
-
-  // Extract keywords and keyphrases from bio and add to existing keywords and keyphrases list
-  const fileBio = await retext()
-    .use(retextPos) // Make sure to use `retext-pos` before `retext-keywords`.
+  const fileReadme = await retext()
+    .use(retextPos)
     .use(retextKeywords)
-    .process(await decodeBio)
+    .process(await decodeReadme);
 
-  keywordsArray = keywordsArray.concat(fileBio.data.keywords.map((keyword) => {
-      return toString(keyword.matches[0].node);
-      }).filter(word => !stopwords.english.includes(word)));
+  keywordsArray.push(
+    ...fileReadme.data.keywords
+      .map((keyword) => toString(keyword.matches[0].node))
+      .filter((word) => !stopwords.english.includes(word))
+  );
 
-  keyphrasesArray = keyphrasesArray.concat(fileBio.data.keyphrases.map((phrase) => {
-      return phrase.matches[0].nodes.map((d) => toString(d)).join('');
-  }).filter(word => !stopwords.english.includes(word)));
+  keyphrasesArray.push(
+    ...fileReadme.data.keyphrases
+      .map((phrase) => phrase.matches[0].nodes.map((d) => toString(d)).join(""))
+      .filter((word) => !stopwords.english.includes(word))
+  );
 
-  // Combine keywords and keyphrases as 1 list
-  const keyProfile = keywordsArray.concat(keyphrasesArray)
+  // bio + readme
+  const keyProfile = [...keywordsArray, ...keyphrasesArray];
 
+  // languages
   let sortedLanguagesCount, languagesPercentage;
   if (token) {
     const dataLanguages = repos.map((r) =>
@@ -124,11 +124,11 @@ const skillInference = async (
   );
 
   return {
+    key_qualifications: keyProfile,
     top_n_languages: topNLanguages,
     languages_percentage: languagesPercentage
       ? languagesPercentage
       : "only available for authorized request",
-    key_qualifications: keyProfile
   };
 };
 
