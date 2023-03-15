dd30edc640e2321a6b6697eb1eb0c04f395ae3a6
geraldbryan <geraldbryan9914@gmail.com>
2023-03-15 15:58:22 +0700

    keywords extraction from list of keywords

--- src/github/skillInference.js
+++ src/github/skillInference.js
@@ -14,6 +14,16 @@ const skillInference = async (
   token = null,
   top_language_n = 3
 ) => {
+
+  // keywords list
+  const keywords = await fetch('https://raw.githubusercontent.com/supertypeai/collective/staging/src/data/profileTagsChoices.json') //temporary in staging branch, for production change itto main
+        .then(response => response.json());
+
+  let labelArr = keywords.map((item) => item.label.toLowerCase());
+  let valueArr = keywords.map((item) => item.value.replace(/-/g, ' ').toLowerCase());
+
+  const keywordsList = [...new Set([...labelArr, ...valueArr])];
+
   let decodeBio, decodeReadme;
 
   // bio
@@ -25,19 +35,9 @@ const skillInference = async (
       )
       .toLowerCase();
   }
-
-  const fileBio = await retext()
-    .use(retextPos)
-    .use(retextKeywords)
-    .process(await decodeBio);
-
-  let keywordsArray = fileBio.data.keywords
-    .map((keyword) => toString(keyword.matches[0].node))
-    .filter((word) => !stopwords.english.includes(word));
-
-  let keyphrasesArray = fileBio.data.keyphrases
-    .map((phrase) => phrase.matches[0].nodes.map((d) => toString(d)).join(""))
-    .filter((word) => !stopwords.english.includes(word));
+    var keyProfile = keywordsList.filter(word =>
+      new RegExp(`\\b${word}\\b`, "i").test(decodeBio)
+  );
 
   // readme
   const responseReadme = await fetch(
@@ -53,25 +53,22 @@ const skillInference = async (
       .toLowerCase();
   }
 
-  const fileReadme = await retext()
-    .use(retextPos)
-    .use(retextKeywords)
-    .process(await decodeReadme);
+  keyProfile = [...new Set(keyProfile.concat(keywordsList.filter(word =>
+      new RegExp(`\\b${word}\\b`, "i").test(decodeContent)
+  )))]
 
-  keywordsArray.push(
-    ...fileReadme.data.keywords
-      .map((keyword) => toString(keyword.matches[0].node))
-      .filter((word) => !stopwords.english.includes(word))
-  );
+  //console log value and label
 
-  keyphrasesArray.push(
-    ...fileReadme.data.keyphrases
-      .map((phrase) => phrase.matches[0].nodes.map((d) => toString(d)).join(""))
-      .filter((word) => !stopwords.english.includes(word))
-  );
+  const commonValues = valueArr.filter(value => matchedWords.includes(value)).map(word => word.replace(/\s/g, '-').replace('/', '-'));
 
-  // bio + readme
-  const keyProfile = [...keywordsArray, ...keyphrasesArray];
+  const capitalizeMatchedWords = matchedWords.map(word => word.split(' ').map(subWord => subWord.charAt(0).toUpperCase() + subWord.slice(1)).join(' '));
+    
+  const valueList = capitalizeMatchedWords.map(str => {
+      const dict = keywords.find(d => d.label === str);
+      return dict ? dict.value : null;
+  }).filter(item => item !== null);
+
+  const valueKeywordsList = [...new Set([...commonValues, ...valueList])];
 
   // languages
   let sortedLanguagesCount, languagesPercentage;
@@ -125,6 +122,7 @@ const skillInference = async (
 
   return {
     key_qualifications: keyProfile,
+    value_keywords: valueKeywordsList,
     top_n_languages: topNLanguages,
     languages_percentage: languagesPercentage
       ? languagesPercentage
