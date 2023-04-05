a68d0a44c083aec0a1e448188de6982d9a22e204
geraldbryan <geraldbryan9914@gmail.com>
2023-04-05 21:19:43 +0700

    add function to infer repository data

--- src/github/index.js
+++ src/github/index.js
@@ -1,2 +1,3 @@
 export { default as inferFromGithub } from "./inferFromGithub.js";
 export { default as languageInference } from "./languageInference.js";
+export { default as repository } from "./repository.js"
